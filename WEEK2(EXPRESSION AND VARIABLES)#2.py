264b13db1dd4dc8b3ed20e5786be3f24e1f5e931
Lyndon SD Yap <84137554+yplyndon@users.noreply.github.com>
2022-02-14 23:36:45 +0800

    Create WEEK2(EXPRESSION AND VARIABLES)#2.py

---
+++ WEEK2(EXPRESSION AND VARIABLES)#2.py
@@ -0,0 +1,10 @@
+'''
+This code is supposed to take two numbers, divide one by another so that the result is equal to 1,
+ and display the result on the screen. Unfortunately, there is an error in the code. 
+ Find the error and fix it, so that the output is correct.
+'''
+
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(result)
