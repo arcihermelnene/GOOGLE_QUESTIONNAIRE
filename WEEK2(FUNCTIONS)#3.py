177b7476fa4655d018301b212906dd41afa5fc1a
Lyndon SD Yap <84137554+yplyndon@users.noreply.github.com>
2022-02-15 00:02:07 +0800

    Create WEEK2(FUNCTIONS)#3.py

---
+++ WEEK2(FUNCTIONS)#3.py
@@ -0,0 +1,15 @@
+'''
+Question 4
+Let's revisit our lucky_number function. 
+We want to change it, so that instead of printing the message,
+ it returns the message. This way, the calling line can print the message,
+  or do something else with it if needed. Fill in the blanks to complete the code to make it work.
+'''
+
+def lucky_number(name):
+  number = len(name) * 9
+  message = "Hello " + name + ". Your lucky number is " + str(number)
+  return message
+	    
+print(lucky_number("Kay"))
+print(lucky_number("Cameron"))
