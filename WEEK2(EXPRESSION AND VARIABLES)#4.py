672e4e3cbd06251d2fc8dda43490bdda56596526
Lyndon SD Yap <84137554+yplyndon@users.noreply.github.com>
2022-02-14 23:36:51 +0800

    Create WEEK2(EXPRESSION AND VARIABLES)#4.py

---
+++ WEEK2(EXPRESSION AND VARIABLES)#4.py
@@ -0,0 +1,7 @@
+'''
+Question 4
+This code is supposed to display "2 + 2 = 4" on the screen, but there is an error.
+ Find the error in the code and fix it, so that the output is correct.
+'''
+
+print("2 + 2 = " + str(2 + 2))
